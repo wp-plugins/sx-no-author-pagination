df605fd8e476884090bdb16750720f1f0813edd4
Confridin <Confridin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-25 11:08:38 +0000

    Adding compatibily check
    git-svn-id: https://plugins.svn.wordpress.org/sx-no-author-pagination/trunk@864633 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sx-no-author-pagination.php
+++ sx-no-author-pagination.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name:  SX No author Pagination
-Version:      1.1
+Version:      1.2
 Plugin URI:   http://www.seomix.fr
 Description:  SX No author Pagination removes properly any author pagination and redirects useless paginated content.
 Availables languages : en_EN
-Tags: author, pagination
+Tags: author, pagination, page, auteur, paginated
 Author: Daniel Roch
 Author URI: http://www.seomix.fr
 Requires at least: 3.3
@@ -50,7 +50,6 @@ add_action('pre_get_posts', 'seomix_remove_author_pagination');
 
 /**
  Redirect author pagination
- * Redirect author pagination
  * © Daniel Roch 
  */ 
 function seomix_redirect_author_pagination () {
@@ -63,3 +62,57 @@ function seomix_redirect_author_pagination () {
   }
 }
 add_action( 'template_redirect', 'seomix_redirect_author_pagination' );
+
+
+/**
+ Check Compatibility
+ * http://wptavern.com/how-to-prevent-wordpress-plugins-from-activating-on-sites-with-incompatible-hosting-environments
+ */
+class seomix_noauthor_checkcompatibility {
+    function __construct() {
+        add_action( 'admin_init', array( $this, 'check_version' ) );
+
+        // Don't run anything else in the plugin, if we're on an incompatible WordPress version
+        if ( ! self::compatible_version() ) {
+            return;
+        }
+    }
+
+    // The primary sanity check, automatically disable the plugin on activation if it doesn't
+    // meet minimum requirements.
+    static function activation_check() {
+        if ( ! self::compatible_version() ) {
+            deactivate_plugins( plugin_basename( __FILE__ ) );
+            wp_die('SX No Author Pagination requires WordPress 3.3 or higher!');
+        }
+    }
+
+    // The backup sanity check, in case the plugin is activated in a weird way,
+    // or the versions change after activation.
+    function check_version() {
+        if ( ! self::compatible_version() ) {
+            if ( is_plugin_active( plugin_basename( __FILE__ ) ) ) {
+                deactivate_plugins( plugin_basename( __FILE__ ) );
+                add_action( 'admin_notices', array( $this, 'disabled_notice' ) );
+                if ( isset( $_GET['activate'] ) ) {
+                    unset( $_GET['activate'] );
+                }
+            }
+        }
+    }
+
+    function disabled_notice() {
+       echo '<div class="updated"><p><strong>SX No Author Pagination requires WordPress 3.3 or higher!</strong></p></div>';
+    }
+
+    static function compatible_version() {
+        if ( version_compare( $GLOBALS['wp_version'], '3.3', '<' ) ) {
+             return false;
+         }
+        return true;
+    }
+}
+
+global $sxnoauthorcheck;
+$sxnoauthorcheck = new seomix_noauthor_checkcompatibility();
+register_activation_hook( __FILE__, array( 'seomix_noauthor_checkcompatibility', 'activation_check' ) );
